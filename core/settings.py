94fd4ae868081508ae7f27e2a160b631451d44ca
alina kamatali <alinakamatali@gmail.com>
2026-06-22 17:55:52 +0300

    added logos to companies

--- core/settings.py
+++ core/settings.py
@@ -137,7 +137,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (user uploads)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 def trigger_error(request):
     return 1 / 0
@@ -15,3 +17,5 @@ urlpatterns = [
     path('silk/', include('silk.urls', namespace='silk')), # для силк
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
